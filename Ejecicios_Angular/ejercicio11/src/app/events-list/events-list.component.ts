056635f53e32a47806038fc087a48c2bcc31587a
PRastelli <pablorastelli@gmail.com>
2022-09-19 18:29:35 -0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   Ejecicios_Angular/ejercicio11/.browserslistrc
    	new file:   Ejecicios_Angular/ejercicio11/.editorconfig
    	new file:   Ejecicios_Angular/ejercicio11/.gitignore
    	new file:   Ejecicios_Angular/ejercicio11/.vscode/extensions.json
    	new file:   Ejecicios_Angular/ejercicio11/.vscode/launch.json
    	new file:   Ejecicios_Angular/ejercicio11/.vscode/tasks.json
    	new file:   Ejecicios_Angular/ejercicio11/README.md
    	new file:   Ejecicios_Angular/ejercicio11/angular.json
    	new file:   Ejecicios_Angular/ejercicio11/karma.conf.js
    	new file:   Ejecicios_Angular/ejercicio11/package-lock.json
    	new file:   Ejecicios_Angular/ejercicio11/package.json
    	new file:   Ejecicios_Angular/ejercicio11/src/app/app.component.css
    	new file:   Ejecicios_Angular/ejercicio11/src/app/app.component.html
    	new file:   Ejecicios_Angular/ejercicio11/src/app/app.component.spec.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/app/app.component.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/app/app.module.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/app/events-list/events-list.component.css
    	new file:   Ejecicios_Angular/ejercicio11/src/app/events-list/events-list.component.html
    	new file:   Ejecicios_Angular/ejercicio11/src/app/events-list/events-list.component.spec.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/app/events-list/events-list.component.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/assets/.gitkeep
    	new file:   Ejecicios_Angular/ejercicio11/src/environments/environment.prod.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/environments/environment.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/favicon.ico
    	new file:   Ejecicios_Angular/ejercicio11/src/index.html
    	new file:   Ejecicios_Angular/ejercicio11/src/main.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/polyfills.ts
    	new file:   Ejecicios_Angular/ejercicio11/src/styles.css
    	new file:   Ejecicios_Angular/ejercicio11/src/test.ts
    	new file:   Ejecicios_Angular/ejercicio11/tsconfig.app.json
    	new file:   Ejecicios_Angular/ejercicio11/tsconfig.json
    	new file:   Ejecicios_Angular/ejercicio11/tsconfig.spec.json

---
+++ Ejecicios_Angular/ejercicio11/src/app/events-list/events-list.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  styleUrls: [ './events-list.component.css' ]
+})
+export class EventsListComponent implements OnInit {
+
+  events = [
+    {
+      name: 'Angular Connect',
+      date: '9/26/2036',
+      time: '10am',
+      location: {
+        address: '1 London Road ',
+        city: 'London',
+        country: 'England'
+      },
+      hidden: true
+    },
+    {
+      name: 'ng-conf 2037',
+      date: '4/15/2037',
+      time: '9am',
+      location: {
+        address: 'The Palatial America Hotel',
+        city: 'Salt Lake City',
+        country: 'USA'
+      },
+      hidden: true
+    },
+    {
+      name: 'UN Angular Summit',
+      date: '6/10/2037',
+      time: '8am',
+      location: {
+        address: 'The UN Angular Center',
+        city: 'New York',
+        country: 'USA'
+      },
+      onlineUrl: 'http://unangularsummit.org',
+      hidden: true
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
