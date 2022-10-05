73fa210c1cf1efe7e9b2244145eb75009817c1c7
PRastelli <pablorastelli@gmail.com>
2022-10-05 20:02:32 -0300

    new file:   Ejecicios_Angular/ejercicio34/.browserslistrc 	new file:   Ejecicios_Angular/ejercicio34/.editorconfig 	new file:   Ejecicios_Angular/ejercicio34/.gitignore 	new file:   Ejecicios_Angular/ejercicio34/.vscode/extensions.json 	new file:   Ejecicios_Angular/ejercicio34/.vscode/launch.json 	new file:   Ejecicios_Angular/ejercicio34/.vscode/tasks.json 	new file:   Ejecicios_Angular/ejercicio34/README.md 	new file:   Ejecicios_Angular/ejercicio34/angular.json 	new file:   Ejecicios_Angular/ejercicio34/karma.conf.js 	new file:   Ejecicios_Angular/ejercicio34/package-lock.json 	new file:   Ejecicios_Angular/ejercicio34/package.json 	new file:   Ejecicios_Angular/ejercicio34/src/app/app.component.css 	new file:   Ejecicios_Angular/ejercicio34/src/app/app.component.html 	new file:   Ejecicios_Angular/ejercicio34/src/app/app.component.spec.ts 	new file:   Ejecicios_Angular/ejercicio34/src/app/app.component.ts 	new file:   Ejecicios_Angular/ejercicio34/src/app/app.module.ts 	new file:   Ejecicios_Angular/ejercicio34/src/app/parent/parent.component.css 	new file:   Ejecicios_Angular/ejercicio34/src/app/parent/parent.component.html 	new file:   Ejecicios_Angular/ejercicio34/src/app/parent/parent.component.spec.ts 	new file:   Ejecicios_Angular/ejercicio34/src/app/parent/parent.component.ts 	new file:   Ejecicios_Angular/ejercicio34/src/assets/.gitkeep 	new file:   Ejecicios_Angular/ejercicio34/src/environments/environment.prod.ts 	new file:   Ejecicios_Angular/ejercicio34/src/environments/environment.ts 	new file:   Ejecicios_Angular/ejercicio34/src/favicon.ico 	new file:   Ejecicios_Angular/ejercicio34/src/index.html 	new file:   Ejecicios_Angular/ejercicio34/src/main.ts 	new file:   Ejecicios_Angular/ejercicio34/src/polyfills.ts 	new file:   Ejecicios_Angular/ejercicio34/src/styles.css 	new file:   Ejecicios_Angular/ejercicio34/src/test.ts 	new file:   Ejecicios_Angular/ejercicio34/tsconfig.app.json 	new file:   Ejecicios_Angular/ejercicio34/tsconfig.json 	new file:   Ejecicios_Angular/ejercicio34/tsconfig.spec.json

---
+++ Ejecicios_Angular/ejercicio34/src/app/parent/parent.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Movie {
+  name: string;
+  rating: string;
+};
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  movies: Movie[] = [
+    { name:  "Star Wars Episode X", rating: "PG" },
+    { name:  "Rocky XV", rating: "PG-13" },
+    { name:  "Jaws the Revenge", rating: "R" },
+    { name:  "Avengers 6 - The Marriage of the Hulk", rating: "PG-13" },
+    { name:  "Finding Dory's Grandkids", rating: "G" },
+  ];
+
+  displayedMovies: Movie[] = [];
+
+  constructor() {
+    this.displayedMovies = this.movies.slice();
+  }
+
+  ngOnInit() {
+
+  }
+
+  filter(rating: string) {
+    this.displayedMovies = this.movies.filter(data => data.rating === rating);
+  }
+
+}
+
